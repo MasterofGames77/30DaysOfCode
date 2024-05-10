4b48da6f7b6bb0498f98076de78048cd1c12859b
MasterofGames77 <mgambardella16@gmail.com>
2024-05-10 10:49:20 -0400

    Added day 4 challenge

--- day004_Class_VS_Instance.js
+++ day004_Class_VS_Instance.js
@@ -1,1 +1,43 @@
 // Class vs. Instance
+
+function Person(initialAge)
+{
+    // Add some more code to run some checks on initialAge
+    let age;
+    if (initialAge >= 0) 
+        {
+        age = initialAge
+        console.log(`You are ${initialAge} years old.`)
+    } 
+    else 
+    {
+        age = 0
+        console.log('Age is not valid, setting age to 0.')
+    }
+    
+  this.amIOld=function()
+  {
+   // Do some computations in here and print out the correct statement to the console
+   if (age < 13) 
+    {
+       console.log('You are young.')
+   } 
+   else if (age >= 13 && age < 18) 
+    {
+       console.log('You are a teenager.')
+   } 
+   else 
+   {
+       console.log('You are old.')
+   }
+
+  }
+   this.yearPasses=function()
+   {
+          // Increment the age of the person in here
+          age += 1
+   }
+}
+
+console.log(Person(26))
+console.log(amIOld())
