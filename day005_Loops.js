6bc00d732e22433a9fbf4e8079419724c794d693
MasterofGames77 <mgambardella16@gmail.com>
2024-05-11 12:49:12 -0400

    Added day 5 challenge

---
+++ day005_Loops.js
@@ -0,0 +1,13 @@
+// Loops
+
+function main() {
+    const n = parseInt(2);
+    for (let i = 1; i <= 10; i++) {
+        // Testing
+        //let result = n * i
+        //console.log(result)
+        console.log(n.toString() + " x " + i + " = " + result.toString())
+    }
+}
+
+main()
