578e43610197f34b68b784859c3e84fdd39ed70a
MasterofGames77 <mgambardella16@gmail.com>
2024-05-20 12:08:07 -0400

    Added day 14 challenge in TypeScript

---
+++ day014.Scope.ts
@@ -0,0 +1,38 @@
+// Scope
+// Written in TypeScript
+
+'use strict';
+let inputString: string = '';
+let inputLines: string[] = [];
+let currentLine: number = 0;
+
+function readLine(): string {
+    return inputLines[currentLine++];
+}
+
+class Difference {
+    elements: number[];
+    maximumDifference: number;
+
+    constructor(elements: number[]) {
+        this.elements = elements;
+        this.maximumDifference = 0;
+    }
+
+    computeDifference(): void {
+        let maxElement = Math.max(...this.elements);
+        let minElement = Math.min(...this.elements);
+        this.maximumDifference = Math.abs(maxElement - minElement);
+    }
+}
+
+function main() {
+    // Enter your code here
+    let n: number = parseInt(readLine().trim());
+    let elements: number[] = readLine().trim().split(' ').map(Number);
+
+    let difference = new Difference(elements);
+    difference.computeDifference();
+
+    console.log(difference.maximumDifference);
+}
