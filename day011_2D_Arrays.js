5e285f0c4b3683c8606789b76af6362b62e97d8b
MasterofGames77 <mgambardella16@gmail.com>
2024-05-25 13:56:19 -0400

    updated day 11 code

--- day011_2D_Arrays.js
+++ day011_2D_Arrays.js
@@ -1,29 +1,34 @@
 // 2D Arrays
 
+'use strict';
+
 function main() 
 {
+    // 2D array input
+    let arr = [
+        [1, 1, 1, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [1, 1, 1, 0, 0, 0],
+        [0, 0, 2, 4, 4, 0],
+        [0, 0, 0, 2, 0, 0],
+        [0, 0, 1, 2, 4, 0]
+    ];
 
-    let arr = Array(6);
-
-    for (let i = 0; i < 6; i++) 
-        {
-            arr[i] = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10))
-    }
-    
     let sumArray = []
-    for (let i = 0; i < arr.length; i++) 
-        {
-        for (let j = 0; j < arr[i].length; j++) 
-            {
-        if (arr[i] && arr[i + 1] && arr[i + 2]) 
-            {
-            if (arr[i][j + 1] != undefined && arr[i][j + 2] != undefined) 
-                {
-                sumArray.push(arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2])
-                console.log(sumArray)
+
+    for (let i = 0; i < arr.length - 2; i++) 
+      {
+        for (let j = 0; j < arr[i].length - 2; j++) 
+          {
+            let sum = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] // top row
+                      + arr[i + 1][j + 1] // middle element
+                      + arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2] // bottom row
+            sumArray.push(sum)
         }
-      }
     }
-  }
-  console.log(Math.max.apply(null, sumArray));
+
+    // Find the maximum hourglass sum
+    console.log(Math.max(...sumArray))
 }
+
+main()
