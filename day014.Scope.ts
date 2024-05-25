552a42f5a83613ca1ef901dff2a7212e5fbf2616
MasterofGames77 <mgambardella16@gmail.com>
2024-05-25 13:39:31 -0400

    Added day 14 assignment in JavaScript

--- day014.Scope.ts
+++ day014.Scope.ts
@@ -1,38 +1,38 @@
 // Scope
 // Written in TypeScript
 
-'use strict';
-let inputString: string = '';
-let inputLines: string[] = [];
-let currentLine: number = 0;
+// 'use strict';
+// let inputString: string = '';
+// let inputLines: string[] = [];
+// let currentLine: number = 0;
 
-function readLine(): string {
-    return inputLines[currentLine++];
-}
+// function readLine(): string {
+//     return inputLines[currentLine++];
+// }
 
-class Difference {
-    elements: number[];
-    maximumDifference: number;
+// class Difference {
+//     elements: number[];
+//     maximumDifference: number;
 
-    constructor(elements: number[]) {
-        this.elements = elements;
-        this.maximumDifference = 0;
-    }
+//     constructor(elements: number[]) {
+//         this.elements = elements;
+//         this.maximumDifference = 0;
+//     }
 
-    computeDifference(): void {
-        let maxElement = Math.max(...this.elements);
-        let minElement = Math.min(...this.elements);
-        this.maximumDifference = Math.abs(maxElement - minElement);
-    }
-}
+//     computeDifference(): void {
+//         let maxElement = Math.max(...this.elements);
+//         let minElement = Math.min(...this.elements);
+//         this.maximumDifference = Math.abs(maxElement - minElement);
+//     }
+// }
 
-function main() {
-    // Enter your code here
-    let n: number = parseInt(readLine().trim());
-    let elements: number[] = readLine().trim().split(' ').map(Number);
+// function main() {
+//     // Enter your code here
+//     let n: number = parseInt(readLine().trim());
+//     let elements: number[] = readLine().trim().split(' ').map(Number);
 
-    let difference = new Difference(elements);
-    difference.computeDifference();
+//     let difference = new Difference(elements);
+//     difference.computeDifference();
 
-    console.log(difference.maximumDifference);
-}
+//     console.log(difference.maximumDifference);
+// }
---
+++ day014_Scope.js
@@ -0,0 +1,28 @@
+class Difference 
+{
+    constructor(elements) 
+    {
+        this.elements = elements;
+        this.maximumDifference = 0;
+    }
+
+    computeDifference() 
+    {
+        let maxElement = Math.max(...this.elements);
+        let minElement = Math.min(...this.elements);
+        this.maximumDifference = Math.abs(maxElement - minElement);
+    }
+}
+
+function main() 
+{
+    let n = parseInt(3)
+    let elements = [1, 2, 5]
+
+    let difference = new Difference(elements);
+    difference.computeDifference();
+
+    console.log(difference.maximumDifference);
+}
+
+console.log(main())
