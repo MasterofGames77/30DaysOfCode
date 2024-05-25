0db98e6c4f8559b619dfeb9c9fa33d4e8f7e824e
MasterofGames77 <mgambardella16@gmail.com>
2024-05-25 14:02:10 -0400

    updated day 8 code

--- day008_Dictionaries_Maps.js
+++ day008_Dictionaries_Maps.js
@@ -2,23 +2,40 @@
 
 function processData(input) 
 {
+    // Enter your code here
     input = input.split('\n')
-    let phoneBook = []
+    let phoneBook = {}
 
-    for (let i = 1; i < parseInt(input[0]); i++) 
+    for (let i = 1; i <= parseInt(input[0]); i++) 
         {
-            let contactArray = input[i].split(' ')
-            phoneBook[contactArray[0]] = contactArray[1]
+        let contactArray = input[i].split(' ')
+        phoneBook[contactArray[0]] = contactArray[1]
     }
 
     for (let i = (parseInt(input[0]) + 1); i < input.length; i++) 
         {
-            if (phoneBook[input[i]]) 
-                {
-                    console.log(input[i] + '=' + phoneBook[input[i]])
-            } 
-            else {
-                console.log('Not found')
-            }
+        if (phoneBook[input[i]]) 
+            {
+            console.log(input[i] + '=' + phoneBook[input[i]])
+        } 
+        else 
+        {
+            console.log('Not found')
+        }
     }
 }
+
+function main() 
+{
+    const input = `3
+sam 99912222
+tom 11122222
+harry 12299933
+sam
+edward
+harry`;
+
+    processData(input)
+}
+
+main()
